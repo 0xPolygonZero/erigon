02d0f07e72cf8b98abc2be364ffb346ef42e2c0f
Manav Darji <manavdarji.india@gmail.com>
2024-03-19 00:37:00 +0530

    refactor eth_getWitness to consume common functions for witness generation

--- turbo/jsonrpc/eth_call.go
+++ turbo/jsonrpc/eth_call.go
@@ -421,7 +421,6 @@ func (api *BaseAPI) getWitness(ctx context.Context, db kv.RoDB, blockNrOrHash rp
 	}
 
 	blockNr, hash, _, err := rpchelper.GetCanonicalBlockNumber(blockNrOrHash, tx, api.filters) // DoCall cannot be executed on non-canonical blocks
-
 	if err != nil {
 		return nil, err
 	}
@@ -447,11 +446,6 @@ func (api *BaseAPI) getWitness(ctx context.Context, db kv.RoDB, blockNrOrHash rp
 		return nil, nil
 	}
 
-	prevHeader, err := api._blockReader.HeaderByNumber(ctx, tx, blockNr-1)
-	if err != nil {
-		return nil, err
-	}
-
 	latestBlock, err := rpchelper.GetLatestBlockNumber(tx)
 	if err != nil {
 		return nil, err
@@ -462,45 +456,43 @@ func (api *BaseAPI) getWitness(ctx context.Context, db kv.RoDB, blockNrOrHash rp
 		return nil, fmt.Errorf("block number is in the future latest=%d requested=%d", latestBlock, blockNr)
 	}
 
-	rl := trie.NewRetainList(0)
-
-	regenerate_hash := false
-	if latestBlock-blockNr > uint64(maxGetProofRewindBlockCount) {
-		regenerate_hash = true
+	// Check if the witness is present in the database. If found, return it.
+	wBytes, err := stagedsync.ReadChunks(tx, kv.Witnesses, stagedsync.Uint64ToBytes(blockNr))
+	if err == nil && wBytes != nil {
+		return wBytes, nil
 	}
 
-	if blockNr-1 < latestBlock {
-		batch := membatchwithdb.NewMemoryBatch(tx, api.dirs.Tmp, logger)
-		defer batch.Rollback()
-
-		unwindState := &stagedsync.UnwindState{UnwindPoint: blockNr - 1}
-		stageState := &stagedsync.StageState{BlockNumber: latestBlock}
-
-		hashStageCfg := stagedsync.StageHashStateCfg(nil, api.dirs, api.historyV3(batch))
-		if err := stagedsync.UnwindHashStateStage(unwindState, stageState, batch, hashStageCfg, ctx, logger); err != nil {
-			return nil, err
-		}
-
-		if !regenerate_hash {
-			interHashStageCfg := stagedsync.StageTrieCfg(nil, false, false, false, api.dirs.Tmp, api._blockReader, nil, api.historyV3(batch), api._agg)
-			err = stagedsync.UnwindIntermediateHashes("eth_getWitness", rl, unwindState, stageState, batch, interHashStageCfg, ctx.Done(), logger)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			_ = batch.ClearBucket(kv.TrieOfAccounts)
-			_ = batch.ClearBucket(kv.TrieOfStorage)
-		}
-
-		tx = batch
-	}
-
-	chainConfig, err := api.chainConfig(tx)
+	// If witness is not present in the db, or there's some error reading it, compute it
+	prevHeader, err := api._blockReader.HeaderByNumber(ctx, tx, blockNr-1)
 	if err != nil {
 		return nil, err
 	}
 
-	header := block.Header()
+	regenerateHash := false
+	if latestBlock-blockNr > uint64(maxGetProofRewindBlockCount) {
+		regenerateHash = true
+	}
+
+	var rl *trie.RetainList
+	batch := membatchwithdb.NewMemoryBatch(tx, "", logger)
+	defer batch.Rollback()
+
+	engine, ok := api.engine().(consensus.Engine)
+	if !ok {
+		return nil, fmt.Errorf("engine is not consensus.Engine")
+	}
+
+	// Prepare witness config
+	chainConfig := api._chainConfig.Load()
+	cfg := stagedsync.StageWitnessCfg(nil, true, 0, chainConfig, engine, api._blockReader, api.dirs)
+
+	batch, rl, err = stagedsync.RewindStagesForWitness(batch, blockNr, &cfg, regenerateHash, ctx, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update the tx to operate on the in-memory batch
+	tx = batch
 
 	reader, err := rpchelper.CreateHistoryStateReader(tx, blockNr, 0, false, chainConfig.ChainName)
 	if err != nil {
@@ -516,6 +508,11 @@ func (api *BaseAPI) getWitness(ctx context.Context, db kv.RoDB, blockNrOrHash rp
 
 	statedb := state.New(tds)
 
+	chainReader := stagedsync.NewChainReaderImpl(chainConfig, tx, api._blockReader, logger)
+	if err := core.InitializeBlockExecution(engine, chainReader, block.Header(), chainConfig, statedb, trieStateWriter, nil); err != nil {
+		return nil, err
+	}
+
 	getHeader := func(hash libcommon.Hash, number uint64) *types.Header {
 		h, e := api._blockReader.Header(ctx, tx, hash, number)
 		if e != nil {
@@ -523,105 +520,14 @@ func (api *BaseAPI) getWitness(ctx context.Context, db kv.RoDB, blockNrOrHash rp
 		}
 		return h
 	}
-
 	getHashFn := core.GetHashFn(block.Header(), getHeader)
 
-	usedGas := new(uint64)
-	usedBlobGas := new(uint64)
-	gp := new(core.GasPool).AddGas(block.GasLimit()).AddBlobGas(chainConfig.GetMaxBlobGasPerBlock())
-	var receipts types.Receipts
-
-	engine, ok := api.engine().(consensus.Engine)
-
-	if !ok {
-		return nil, fmt.Errorf("engine is not consensus.Engine")
-	}
-
-	chainReader := stagedsync.NewChainReaderImpl(chainConfig, tx, nil, nil)
-
-	if err := core.InitializeBlockExecution(engine, chainReader, block.Header(), chainConfig, statedb, trieStateWriter, nil); err != nil {
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	if len(block.Transactions()) == 0 {
-		statedb.GetBalance(libcommon.HexToAddress("0x1234"))
-	}
-
-	vmConfig := vm.Config{}
-
-	for i, txn := range block.Transactions() {
-		statedb.SetTxContext(txn.Hash(), block.Hash(), i)
-		receipt, _, err := core.ApplyTransaction(chainConfig, getHashFn, api.engine(), nil, gp, statedb, trieStateWriter, block.Header(), txn, usedGas, usedBlobGas, vmConfig)
-		if err != nil {
-			return nil, err
-		}
-
-		if !chainConfig.IsByzantium(block.NumberU64()) {
-			tds.StartNewBuffer()
-		}
-
-		receipts = append(receipts, receipt)
-	}
-
-	if _, _, _, err = engine.FinalizeAndAssemble(chainConfig, block.Header(), statedb, block.Transactions(), block.Uncles(), receipts, block.Withdrawals(), nil, nil, nil, nil); err != nil {
-		fmt.Printf("Finalize of block %d failed: %v\n", blockNr, err)
-		return nil, err
-	}
-
-	statedb.FinalizeTx(chainConfig.Rules(block.NumberU64(), block.Header().Time), trieStateWriter)
-
-	loadFunc := func(loader *trie.SubTrieLoader, rl *trie.RetainList, dbPrefixes [][]byte, fixedbits []int, accountNibbles [][]byte) (trie.SubTries, error) {
-		rl.Rewind()
-		receiver := trie.NewSubTrieAggregator(nil, nil, false)
-		receiver.SetRetainList(rl)
-		pr := trie.NewMultiAccountProofRetainer(rl)
-		pr.AccHexKeys = accountNibbles
-		receiver.SetProofRetainer(pr)
-
-		loaderRl := rl
-		if regenerate_hash {
-			loaderRl = trie.NewRetainList(0)
-		}
-		subTrieloader := trie.NewFlatDBTrieLoader[trie.SubTries]("eth_getWitness", loaderRl, nil, nil, false, receiver)
-		subTries, err := subTrieloader.Result(tx, nil)
-
-		rl.Rewind()
-
-		if err != nil {
-			return receiver.EmptyResult(), err
-		}
-
-		err = trie.AttachRequestedCode(tx, loader.CodeRequests())
-
-		if err != nil {
-			return receiver.EmptyResult(), err
-		}
-
-		// Reverse the subTries.Hashes and subTries.roots
-		for i, j := 0, len(subTries.Hashes)-1; i < j; i, j = i+1, j-1 {
-			subTries.Hashes[i], subTries.Hashes[j] = subTries.Hashes[j], subTries.Hashes[i]
-			subTries.Roots()[i], subTries.Roots()[j] = subTries.Roots()[j], subTries.Roots()[i]
-		}
-
-		return subTries, nil
-	}
-
-	triePreroot := tds.LastRoot()
-
-	if !bytes.Equal(prevHeader.Root[:], triePreroot[:]) {
-		return nil, fmt.Errorf("mismatch in expected state root computed %v vs %v indicates bug in witness implementation", prevHeader.Root, triePreroot)
-	}
-
-	if err := tds.ResolveStateTrieWithFunc(loadFunc); err != nil {
-		return nil, err
-	}
-
-	w, err := tds.ExtractWitness(false, false)
-
+	w, err := stagedsync.GenerateWitness(tx, block, prevHeader, tds, trieStateWriter, statedb, getHashFn, &cfg, regenerateHash, ctx, logger)
 	if err != nil {
 		return nil, err
+	}
+	if w == nil {
+		return nil, fmt.Errorf("unable to generate witness for block %d", blockNr)
 	}
 
 	var buf bytes.Buffer
@@ -630,85 +536,9 @@ func (api *BaseAPI) getWitness(ctx context.Context, db kv.RoDB, blockNrOrHash rp
 		return nil, err
 	}
 
-	nw, err := trie.NewWitnessFromReader(bytes.NewReader(buf.Bytes()), false)
-
+	err = stagedsync.VerifyWitness(tx, block, prevHeader, chainReader, tds, getHashFn, &cfg, &buf, logger)
 	if err != nil {
-		return nil, err
-	}
-
-	s, err := state.NewStateless(prevHeader.Root, nw, blockNr-1, false, false /* is binary */)
-	if err != nil {
-		return nil, err
-	}
-	ibs := state.New(s)
-	s.SetBlockNr(blockNr)
-
-	gp = new(core.GasPool).AddGas(block.GasLimit()).AddBlobGas(chainConfig.GetMaxBlobGasPerBlock())
-	usedGas = new(uint64)
-	usedBlobGas = new(uint64)
-	receipts = types.Receipts{}
-
-	if err := core.InitializeBlockExecution(engine, chainReader, block.Header(), chainConfig, ibs, s, nil); err != nil {
-		return nil, err
-	}
-
-	for i, txn := range block.Transactions() {
-		ibs.SetTxContext(txn.Hash(), block.Hash(), i)
-		receipt, _, err := core.ApplyTransaction(chainConfig, getHashFn, engine, nil, gp, ibs, s, header, txn, usedGas, usedBlobGas, vmConfig)
-		if err != nil {
-			return nil, fmt.Errorf("tx %x failed: %v", txn.Hash(), err)
-		}
-		receipts = append(receipts, receipt)
-	}
-
-	receiptSha := types.DeriveSha(receipts)
-	if !vmConfig.StatelessExec && chainConfig.IsByzantium(header.Number.Uint64()) && !vmConfig.NoReceipts && receiptSha != block.ReceiptHash() {
-		return nil, fmt.Errorf("mismatched receipt headers for block %d (%s != %s)", block.NumberU64(), receiptSha.Hex(), block.ReceiptHash().Hex())
-	}
-
-	if !vmConfig.StatelessExec && *usedGas != header.GasUsed {
-		return nil, fmt.Errorf("gas used by execution: %d, in header: %d", *usedGas, header.GasUsed)
-	}
-
-	if header.BlobGasUsed != nil && *usedBlobGas != *header.BlobGasUsed {
-		return nil, fmt.Errorf("blob gas used by execution: %d, in header: %d", *usedBlobGas, *header.BlobGasUsed)
-	}
-
-	var bloom types.Bloom
-	if !vmConfig.NoReceipts {
-		bloom = types.CreateBloom(receipts)
-		if !vmConfig.StatelessExec && bloom != header.Bloom {
-			return nil, fmt.Errorf("bloom computed by execution: %x, in header: %x", bloom, header.Bloom)
-		}
-	}
-
-	if !vmConfig.ReadOnly {
-		_, _, _, err := engine.FinalizeAndAssemble(chainConfig, block.Header(), ibs, block.Transactions(), block.Uncles(), receipts, block.Withdrawals(), nil, nil, nil, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		rules := chainConfig.Rules(block.NumberU64(), header.Time)
-
-		ibs.FinalizeTx(rules, s)
-
-		if err := ibs.CommitBlock(rules, s); err != nil {
-			return nil, fmt.Errorf("committing block %d failed: %v", block.NumberU64(), err)
-		}
-	}
-
-	if err = s.CheckRoot(header.Root); err != nil {
-		return nil, err
-	}
-
-	roots, err := tds.UpdateStateTrie()
-
-	if err != nil {
-		return nil, err
-	}
-
-	if roots[len(roots)-1] != block.Root() {
-		return nil, fmt.Errorf("mismatch in expected state root computed %v vs %v indicates bug in witness implementation", roots[len(roots)-1], block.Root())
+		return nil, fmt.Errorf("error verifying witness for block %d: %v", blockNr, err)
 	}
 
 	return buf.Bytes(), nil
